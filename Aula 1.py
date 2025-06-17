60b87484b9cc3048d355049e37be7506df954560
guilhotina-xx <guilhermesantiagoalunosesi@gmail.com>
2025-06-17 12:32:19 -0300

    Meu primeiro Commit

---
+++ Aula 1.py
@@ -0,0 +1,5 @@
+# É uma coleçao nao ordenada de valores UNICOS separados por virgula e dentro de chaves {}
+fruta = {'Maçã','Banana','Abacaxi','Uva'}
+print(fruta)
+fruta = set()
+# conjunto vazio = set
